24fb0c353c559227416bf1d98e5f12467e88fd28
00MB <s2061204@ed.ac.uk>
2021-01-15 11:14:24 +0000

    Week 1 Ex 4

---
+++ Week1/4-Multiplier/src/Multiplier.java
@@ -0,0 +1,8 @@
+public class Multiplier {
+    public static void main( String args[]) {
+        int Number1 = Integer.parseInt(args[0]);
+        int Number2 = Integer.parseInt(args[1]);
+        int Number3 = Integer.parseInt(args[2]);
+        System.out.println(Number1 + " * " + Number2 + " * " + Number3 + " = " + (Number1 * Number2 * Number3));
+    }
+}
