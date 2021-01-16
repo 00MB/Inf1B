87a1d71f2ceb6e2019f377c749e46e25af6c0780
00MB <s2061204@ed.ac.uk>
2021-01-16 09:11:54 +0000

    Week 2 Ex 5

---
+++ Week2/5-QuadraticEquations/src/QuadraticSolver.java
@@ -0,0 +1,12 @@
+public class QuadraticSolver {
+    public static void main( String[] args) {
+        double Num1 = Double.parseDouble(args[0]);
+        double Num2 = Double.parseDouble(args[1]);
+        double Num3 = Double.parseDouble(args[2]);
+        double helper = Math.pow(Num2, 2) - (4 * Num1 * Num3);
+        if (helper >= 0) {
+            System.out.println(((0-Num2)+(Math.sqrt(helper)))/(2 * Num1));
+            System.out.println(((0-Num2)-(Math.sqrt(helper)))/(2 * Num1));
+        }
+    }
+}
