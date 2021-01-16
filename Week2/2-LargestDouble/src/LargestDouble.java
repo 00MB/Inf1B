afbe70e59f11c6919c7f5117a751d03a56e1dc77
00MB <s2061204@ed.ac.uk>
2021-01-16 08:29:57 +0000

    Week 2 Ex 2

---
+++ Week2/2-LargestDouble/src/LargestDouble.java
@@ -0,0 +1,7 @@
+public class LargestDouble {
+    public static void main( String[] args) {
+        double Num1 = Double.parseDouble(args[0]);
+        double Num2 = Double.parseDouble(args[1]);
+        System.out.println(Math.max(Num1, Num2));
+    }
+}
