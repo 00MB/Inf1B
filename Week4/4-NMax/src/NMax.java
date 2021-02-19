3f2810a9ff514811b3dfbbca381b5b70b934dcc6
00MB <s2061204@ed.ac.uk>
2021-02-19 18:30:21 +0000

    Week 4 Ex 5

---
+++ Week4/4-NMax/src/NMax.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class NMax {
+    public static int max(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        }
+        return c;
+    }
+
+    public static double max(double a, double b, double c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        }
+        return c;
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int a = stdIn.nextInt();
+        int b = stdIn.nextInt();
+        int c = stdIn.nextInt();
+        System.out.println(max(a, b, c));
+    }
+}
